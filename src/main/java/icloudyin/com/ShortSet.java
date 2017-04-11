21c59f978e62aa5a199613f44b4faff66ab87338
yinay <yinay@longhz.com>
2017-04-11 15:46:20 +0800

    add mvn-starter

---
+++ src/main/java/icloudyin/com/ShortSet.java
@@ -0,0 +1,17 @@
+package icloudyin.com;
+
+/**
+ * Created by LLWL Xi`an Team
+ * Author iCloud
+ * Data: 11/04/2017
+ */
+public class ShortSet {
+    public static void main (String[] args) {
+//       Set<Short> s = new HashSet<>();
+//        for (short i = 0; i < 100; i++) {
+//            s.add(i);
+//            s.remove(i - 1);
+//        }
+//        System.out.println(s.size());
+    }
+}
